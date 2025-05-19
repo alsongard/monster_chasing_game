beec6a95bb943e8869b19e06f15b6d5918a207f4
alsongard <alsongard@gmail.com>
2025-05-19 23:39:06 +0300

    GameManager Added Selecting Player successfull

--- game/Assets/Scripts/MainMenuController.cs
+++ game/Assets/Scripts/MainMenuController.cs
@@ -1,11 +1,20 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+
+
     public void PlayGame()
     {
+
+        int selectedCharacter = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+        GameManager.instance.CharIndex = selectedCharacter;
+
         Debug.Log("Back on Clock!! Mamba Mentallity");
+        
         SceneManager.LoadScene("monster_game_scene");
     }
 }
