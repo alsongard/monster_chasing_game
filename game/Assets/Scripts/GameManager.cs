5f52b936a72c745f072a01676a718a8e5c6caaab
alsongard <alsongard@gmail.com>
2025-05-19 23:38:06 +0300

    GameManager Added Selecting Player successfully

---
+++ game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // instance of this class itself
+
+    [SerializeField] private GameObject[] playerCharacter;
+
+    private int _charIndex;
+    public int CharIndex
+    {
+        get { return _charIndex; }
+        set { _charIndex = value; }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // delegators and event
+    // += listenOnEvent
+    // -= removed to prevent memory leaks
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(UnityEngine.SceneManagement.Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "monster_game_scene")
+        {
+            Debug.Log("playerCharacter : " + playerCharacter);
+            Instantiate(playerCharacter[CharIndex]);
+        }
+    }
+}
